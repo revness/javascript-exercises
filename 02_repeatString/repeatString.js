648386ae20c04ede4d9f748c54e5c2d8f0c5b326
Chapman Tang <chapmantang@Chapmans-MacBook-Pro.local>
2023-09-22 13:53:42 +1000

    completed to 03_reverseString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
 
+
+const repeatString = function(string,number) {
+if (number<0) {
+return 'ERROR'
+}
+else {
+let str = ''
+for (let i=0; i<number; i++){
+str += string
 };
+return str}}
+repeatString('hey',3)
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+// convert string to array, reverse array or read from last value, then join
+return string.split("").reverse().join("")
 };
 
 // Do not edit below this line

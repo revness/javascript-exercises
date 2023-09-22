990d7db7966d1315f1270312d1f7850abc8ee005
Chapman Tang <chapmantang@Chapmans-MacBook-Pro.local>
2023-09-22 20:03:47 +1000

    completed up to 06_leapyears

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+if (a<0 || b<0){
+return 'ERROR'
+}
+if (typeof(a) != 'number' || typeof(b) != 'number'){
+return 'ERROR'
+}
+let c=0
+if (a<b){
+for (let i=a; i<b+1; i++){
+c +=i 
 };
+return c}
+else {
+for (let i=b;i<a+1; i++){
+c+=i
+}
+return c
+}}
 
 // Do not edit below this line
 module.exports = sumAll;

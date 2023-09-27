33af2e62ee0447f42dd38e8dc63e215d795adedb
Chapman Tang <chapmantang@Chapmans-MacBook-Pro.local>
2023-09-27 23:32:59 +1000

    comleted up to 11

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  let wordArray = word
+    .toLowerCase()
+    .replace(/[^a-z]/g, "")
+    .split("");
+  let wordArrayReverse = wordArray.slice().reverse();
+  let wordArrayLength = wordArray.length;
+  let isPalindrome = true;
 
+  for (let i = 0; i < wordArrayLength; i++) {
+    if (wordArray[i] !== wordArrayReverse[i]) {
+      isPalindrome = false;
+      break;
+    }
+  }
+
+  return isPalindrome;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function (number_in_sequence) {
+  if (number_in_sequence < 0) return "OOPS";
+  if (number_in_sequence == 0) return 0;
+  if (number_in_sequence == 1) return 1;
+  return fibonacci(number_in_sequence - 1) + fibonacci(number_in_sequence - 2);
 };
 
 // Do not edit below this line

e456af07da46580cfc729e29caf3f33e97641c1c
Chapman Tang <chapmantang@Chapmans-MacBook-Pro.local>
2023-09-28 02:12:05 +1000

    completed up to 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  people.map((person) => {
+    if (!person.yearOfDeath) {
+      person.yearOfDeath = new Date().getFullYear();
+    }
+  });
+  const oldest = people.reduce((oldest, currentPerson) => {
+    const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+    const currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+    return oldestAge < currentAge ? currentPerson : oldest;
+  });
+  return oldest;
 };
 
 // Do not edit below this line
